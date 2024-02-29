050be651938e6cd41b63213cd2bf1723de36c057
YingChingKit <22013083d@connect.polyu.hk>
2024-02-29 15:06:50 +0800

    update catapult_controller

--- main/catapult_controller.h
+++ main/catapult_controller.h
@@ -4,14 +4,18 @@
 #include "servo.h"
 #include "button.h"
 #include "pindef.h"
+#include "laser.h"
 
 #define CATAPULT_TURN_DEFAULT_ANGLE (90)
 #define CATAPULT_WIND_DEFAULT_ANGLE (145)
-#define CATAPULT_LOCK_LOCKED_ANGLE (0)
-#define CATAPULT_LOCK_UNLOCKED_ANGLE (90)
+#define CATAPULT_LOCK_LOCKED_ANGLE (90)
+#define CATAPULT_LOCK_UNLOCKED_ANGLE (0)
 #define CATAPULT_LASER_DEFAULT_ANGLE (0)
 #define CATAPULT_TURN_ANGLE_OFFSET (-40.0F)
 #define CATAPULT_RESET_TIMEOUT_US (1 * 1e6)
+
+#define CATAPULT_TILT_LEFT_ANGLE (50)
+#define CATAPULT_TILT_RIGHT_ANGLE (130)
 
 typedef enum
 {
@@ -33,14 +37,20 @@ typedef struct
         servo_handle_t *laser;
         servo_handle_t *turn;
 
+        laser_handle_t *aiming_laser;
+
         sm_state_t state;
         float wind_angle;
         float turn_angle;
+        float laser_angle;
+        float laser_duty_cycle;
         uint64_t catapult_shot_time;
 
 } catapult_controller_handle_t;
 
 catapult_controller_handle_t *catapult_controller_default_config(catapult_controller_handle_t *handle);
 void catapult_controller_init(catapult_controller_handle_t *handle);
+
+float laser_controller(float wind_angle);
 
 void catapult_controller(catapult_controller_handle_t *handle, button_event_t *event);
